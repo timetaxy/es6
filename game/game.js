94512db704af8b2e39fb8014235a0e2ebf76d5d7
= <fantasyz7@naver.com>
2021-03-19 15:42:42 +0900

    object set

--- game/game.js
+++ game/game.js
@@ -8,3 +8,45 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
+
+var dino = {
+    x: 10,
+    y: 200,
+    width: 50,
+    height: 50,
+    draw() {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    },
+    jump() {
+        this.y -= 1;
+    },
+    fall() {
+        if (this.y < 200) {
+            this.y += 1;
+        }
+    }
+};
+
+class Cactus {
+    constructor() {
+        this.x = 600;
+        this.y = 200;
+        this.width = 50;
+        this.height = 50;
+    }
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(img1, this.x, this.y);
+
+    }
+}
+
+
+
+
+
+var cactus = new Cactus();
