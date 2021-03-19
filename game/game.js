6c189393d8ae296f9fcf70efde1a19dff73bcbce
= <fantasyz7@naver.com>
2021-03-19 15:39:45 +0900

    game canvas setting

---
+++ game/game.js
@@ -0,0 +1,10 @@
+// js game
+// 3 tips
+// 1. html에 네모 원
+// js 캔버스 태그
+// 2. 1초에 60번 코드실행(프레임마다 코드실행) 브라우저 기본 함수
+// 3.collision check 유닛두개 충돌하는지 안하는지 검사
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
